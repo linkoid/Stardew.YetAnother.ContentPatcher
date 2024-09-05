4be696efe216984a594e91355b6abdd2316ecb94
linkoid <36754150+linkoid@users.noreply.github.com>
2024-09-05 13:54:55 -0400

    Try using WebAssemblyPrerendered mode

--- YetAnother.Web/Program.cs
+++ YetAnother.Web/Program.cs
@@ -3,9 +3,17 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using YetAnother.Web;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+if (!builder.RootComponents.Any())
+{
+    builder.RootComponents.Add<App>("#app");
+    builder.RootComponents.Add<HeadOutlet>("head::after");
+}
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+ConfigureServices(builder.Services, builder.HostEnvironment.BaseAddress);
 
 await builder.Build().RunAsync();
+
+static void ConfigureServices(IServiceCollection services, string baseAddress)
+{
+    services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) });
+}
